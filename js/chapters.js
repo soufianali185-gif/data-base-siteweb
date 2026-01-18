59446504ec09c60d84262f8d72b78178c12d0d6b
soufianali185-gif <soufianali185@gmail.com>
2026-01-18 18:00:51 +0100

    Create chapters.js

---
+++ js/chapters.js
@@ -0,0 +1,9 @@
+window.DB_COURSE_CHAPTERS = [
+  { title: "Chapitre 1 : Introduction", minutes: 25, href: "chap1-introduction.html" },
+  { title: "Chapitre 2 : Conception – Modèle Entité/Association", minutes: 35, href: "chap2-ea.html" },
+  { title: "Chapitre 3 : Modèle relationnel", minutes: 35, href: "chap3-relationnel.html" },
+  { title: "Chapitre 4 : Dépendances fonctionnelles et normalisation", minutes: 40, href: "chap4-normalisation.html" },
+  { title: "Chapitre 5 : Algèbre relationnelle", minutes: 30, href: "chap5-algebre.html" },
+  { title: "Chapitre 6 : Langage SQL", minutes: 45, href: "chap6-sql.html" },
+
+];
